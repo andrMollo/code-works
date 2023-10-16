9167ab1dc43e3dbf1f597881c6fe14c689f17302
Andrea Mollo <andr.mollo@gmail.com>
2023-10-16 11:56:21 +0200

    Model validation to set the author

--- CodeWorksLibrary/Macros/Properties/SetAuthorMacro.cs
+++ CodeWorksLibrary/Macros/Properties/SetAuthorMacro.cs
@@ -1,12 +1,34 @@
-﻿using CADBooster.SolidDna;
+﻿using SolidWorks.Interop.sldworks;
+using CADBooster.SolidDna;
+using static CADBooster.SolidDna.SolidWorksEnvironment;
+using SolidWorks.Interop.swconst;
 
 namespace CodeWorksLibrary
 {
     internal class SetAuthorMacro
     {
+        /// <summary>
+        /// Write the author in a custom property of the selected components or of the active model if the selection is empty
+        /// </summary>
         public static void SetAuthor()
         {
-            SolidWorksEnvironment.Application.ShowMessageBox("Set author");
+            // The instance to the active model
+            ModelDoc2 swModel = (ModelDoc2)AddIn.swApp.ActiveDoc;
+
+            #region Validation
+            // Check if there is an open document and if there is it can't be a drawing
+            if (swModel is null)
+            {
+                Application.ShowMessageBox("Open a document to set the author",SolidWorksMessageBoxIcon.Stop);
+                return;
+            }
+            if (swModel.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                Application.ShowMessageBox("Open a part or an assembly to set the author", SolidWorksMessageBoxIcon.Stop);
+                return;
+            }
+            #endregion
+
         }
     }
 }
