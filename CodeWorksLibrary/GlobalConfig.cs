e5d50dc6890d2c27933fe6778174c8a595b9e23c
Andrea Mollo <andr.mollo@gmail.com>
2023-10-16 14:30:48 +0200

    Add property name for author

--- CodeWorksLibrary/GlobalConfig.cs
+++ CodeWorksLibrary/GlobalConfig.cs
@@ -6,5 +6,10 @@
         /// The name of the Vault database
         /// </summary>
         public const string VaultName = GlobalConfigPrivate.MyVaultName;
+
+        /// <summary>
+        /// The name of the custom property that contains the author of the file
+        /// </summary>
+        public const string AuthorPropName = "Disegnatore";
     }
 }
