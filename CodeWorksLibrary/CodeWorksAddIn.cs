a0002a1eaec7529240b7b687d23417cff2d3fb20
Andrea Mollo <andr.mollo@gmail.com>
2023-10-16 10:51:13 +0200

    Add command for set author

--- CodeWorksLibrary/CodeWorksAddIn.cs
+++ CodeWorksLibrary/CodeWorksAddIn.cs
@@ -1,15 +1,43 @@
 ﻿using CADBooster.SolidDna;
+using System;
+using System.ComponentModel;
+using Xarial.XCad.Base.Attributes;
+using Xarial.XCad.UI.Commands;
 
 namespace CodeWorksLibrary
 {
     [System.Runtime.InteropServices.ComVisible(true)]
     public class AddIn : Xarial.XCad.SolidWorks.SwAddInEx
     {
+        /// <summary>
+        /// Enumeration that contains the commands to be added to SolidWorks
+        /// </summary>
+        [Title("CodeWorks")]
+        [Description("A collection of macros for SolidWorks")]
+        public enum CwCommands_e
+        {
+            [Title("Set author")]
+            [Description("Write the component author in the custom properties")]
+            SetAuthorE
+        }
+
+        /// <summary>
+        /// Handle the connection to SolidWorks
+        /// </summary>
         public override void OnConnect()
         {
             AddInIntegration.ConnectToActiveSolidWorks(this.Application.Sw.RevisionNumber(), this.AddInId);
 
-            Application.ShowMessageBox("Hello xCAD");
+            CommandManager.AddCommandGroup<CwCommands_e>().CommandClick += OnCommandClick;
+        }
+
+        private void OnCommandClick(CwCommands_e spec)
+        {
+            switch (spec)
+            {
+                case CwCommands_e.SetAuthorE:
+                    break;
+            }
         }
     }
 }
