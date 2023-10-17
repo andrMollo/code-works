6e7925954dc568a6a1dd213aa0bfcc40df0a38eb
Andrea Mollo <andr.mollo@gmail.com>
2023-10-17 07:48:14 +0200

    Add new class and method to export file

---
+++ CodeWorksLibrary/Macros/Files/ExportFileMacro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWorksLibrary.Macros.Files
+{
+    internal class ExportFileMacro
+    {
+        /// <summary>
+        /// Export the open file in different format
+        /// </summary>
+        public static void ExportFile()
+        {
+            /*
+             TODOs:                         
+             Check if there is an open file 
+             Check if the file is saved     
+             Check what type of file is open
+             Process drawing
+              Export drawing
+                  Update sheet format for all sheet
+                  Export to PDF
+                  Export to DWG
+              Get root model
+              Export model
+             Process model
+              Export model
+                  Export model to STEP
+                  Export model to PNG
+              Check if drawing exists
+              Open drawing
+              Process drawing
+             Export
+              Compose output filename
+              Create directory is not present
+            */
+        }
+    }
+}
