901563b634e653ee8eff71b3ea813d42fc0e834b
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-10-22 20:28:30 +0200

    Get the path to the new sheet format

--- CodeWorksLibrary/Macros/Drawings/UpdateFormatMacro.cs
+++ CodeWorksLibrary/Macros/Drawings/UpdateFormatMacro.cs
@@ -61,14 +61,43 @@ namespace CodeWorksLibrary.Macros.Drawings
             }
         }
 
+        /// <summary>
+        /// Get the path of the new sheet format according to the replace map
+        /// </summary>
+        /// <param name="swSheet">The sheet that need a sheet format replace</param>
+        /// <returns>The string with path to the new sheet format</returns>
         private static string GetReplaceSheetFormat(Sheet swSheet)
         {
+            string targetTemplatePath = "";
+
             // Read replace map
             string[] replaceMap = File.ReadAllLines(GlobalConfig.SheetFormatMapPath);
 
-            string targetTemplateName = "";
+            // Get sheet size
+            var currentSize = swSheet.GetSize(-1, -1);
 
-            return targetTemplateName;
+            // Loop through the replace map
+            for (int i = 0; i < replaceMap.Length; i++)
+            {
+                var map = replaceMap[i];
+
+                // Split the replace map line
+                string[] mapParameters = map.Split('|');
+
+                // Assign the paper size in the map to a variable
+                Int32.TryParse(mapParameters[0].Trim(), out int mapPaperSize);
+
+                // Check if there is match in the replace map with the current sheet size
+                if (currentSize == mapPaperSize)
+                {
+                    // Assign the path of the new sheet format
+                    targetTemplatePath = mapParameters[2];
+
+                    Application.ShowMessageBox("Matching size found: " + mapParameters[2], SolidWorksMessageBoxIcon.Information);
+                }
+            }
+
+            return targetTemplatePath;
         }
     }
 }
