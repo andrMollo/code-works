2e5737d19abbd4e242f6546704adfadd33bc0387
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-01 17:27:51 +0100

    Change layer visibility and create layer

--- CodeWorksLibrary/Helpers/CwLayerManager.cs
+++ CodeWorksLibrary/Helpers/CwLayerManager.cs
@@ -9,9 +9,37 @@ namespace CodeWorksLibrary.Helpers
 {
     internal class CwLayerManager
     {
+        /// <summary>
+        /// Toggle the layer visibility
+        /// </summary>
+        /// <param name="swModel"></param>
+        /// <param name="layerName"></param>
+        /// <returns></returns>
         internal static bool ChangeLayerVisibility(ModelDoc2 swModel, string layerName)
         {
+            // Get the model layer manager
+            LayerMgr swLayerMgr = (LayerMgr)swModel.GetLayerManager();
+
+            // Try to get the layer
+            var swLayer = swLayerMgr.GetLayer(layerName);
+
+            if (swLayer == null)
+            {
+                // Create the layer
+            }
+            
+            // Change layer visibility
+
             return true;
+        }
+
+        /// <summary>
+        /// Create a new layer
+        /// </summary>
+        /// <returns></returns>
+        internal static Layer CreateLayer()
+        {
+            return null;
         }
     }
 }
