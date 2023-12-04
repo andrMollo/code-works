dbfcd67bcfaab6bde24dfa7478759b58f91eb8f7
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-04 16:12:16 +0100

    Layer visibility: toggle or change it

--- CodeWorksLibrary/Helpers/CwLayerManager.cs
+++ CodeWorksLibrary/Helpers/CwLayerManager.cs
@@ -12,7 +12,7 @@ namespace CodeWorksLibrary.Helpers
         /// <param name="swModel">The pointer to the model</param>
         /// <param name="layerName">THe name of the layer to be changed</param>
         /// <returns>True if the layer is made visible, false if the layer is hidden</returns>
-        internal static bool ChangeLayerVisibility(ModelDoc2 swModel, string layerName)
+        internal static bool ToggleLayerVisibility(ModelDoc2 swModel, string layerName)
         {
             // Get the model layer manager
             LayerMgr swLayerMgr = (LayerMgr)swModel.GetLayerManager();
@@ -43,6 +43,46 @@ namespace CodeWorksLibrary.Helpers
 
             return false;
         }
+
+        /// <summary>
+        /// Toggle the layer visibility
+        /// </summary>
+        /// <param name="swModel">The pointer to the model</param>
+        /// <param name="layerName">THe name of the layer to be changed</param>
+        /// <param name="layerVisibility">True to make the layer visible, false to hidden it</param>
+        /// <returns>True if the layer is made visible, false if the layer is hidden</returns>
+        internal static bool ChangeLayerVisibility(ModelDoc2 swModel, string layerName, bool layerVisibility)
+        {
+            // Get the model layer manager
+            LayerMgr swLayerMgr = (LayerMgr)swModel.GetLayerManager();
+
+            // Try to get the layer
+            Layer swLayer = (Layer)swLayerMgr.GetLayer(layerName);
+
+            if (swLayer == null)
+            {
+                // Create the layer
+                swLayer = CreateLayer(swModel, layerName);
+            }
+
+            // Change layer visibility
+            if (swLayer != null)
+            {
+                if (layerVisibility == true)
+                {
+                    swLayer.Visible = false;
+                    return false;
+                }
+                else if (layerVisibility == false)
+                {
+                    swLayer.Visible = true;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
 
         /// <summary>
         /// Create a new layer
