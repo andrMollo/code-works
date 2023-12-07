7e1d4e5423392f3eeb0ac25b3c0f7e9f93fe8ff0
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-07 17:24:56 +0100

    Add assembly validation and get configuration

--- CodeWorksLibrary/Macros/Export/ExportAssyMacro.cs
+++ CodeWorksLibrary/Macros/Export/ExportAssyMacro.cs
@@ -4,14 +4,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeWorksLibrary.Helpers;
+using SolidWorks.Interop.sldworks;
 
 namespace CodeWorksLibrary.Macros.Export
 {
     internal class ExportAssyMacro
     {
+        internal static CwBomManager.Bom Bom { get; set; }
+
         internal static void ExportAssembly()
         {
+            #region Validation
             var model = Application.ActiveModel;
+
+            var isAssemblyOpen = CwValidation.AssemblyIsOpen(model);
+
+            if (isAssemblyOpen == false)
+            {
+                return;
+            }
+            #endregion
+
+            // Get the assembly object
+            var swAssy = (AssemblyDoc)model;
+
+            // Get the active configuration
+            var swConf = model.UnsafeObject.ConfigurationManager.ActiveConfiguration;
+
+            // Get the root component
+            var rootComp = swConf.GetRootComponent3(true);
+
+            // Get the flat bom
+            CwBomManager.GetFlatBOM(Bom);
+
         }
     }
 }
