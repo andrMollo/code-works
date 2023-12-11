3f81fbd4fc4035977b5fadcdb2a02f532387a907
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-11 12:14:10 +0100

    Assembly model class

---
+++ CodeWorksLibrary/Models/AssemblyModel.cs
@@ -0,0 +1,15 @@
+﻿using SolidWorks.Interop.sldworks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWorksLibrary.Models
+{
+    internal class AssemblyModel
+    {
+        internal ModelDoc2 Model {  get; set; }
+        internal string Quantity { get; set; }
+    }
+}
