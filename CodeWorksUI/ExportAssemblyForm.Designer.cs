4c73ac6b0989092414bb39f78b86ef7f1795f633
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-13 17:56:53 +0100

    Add export assembly empty form

--- CodeWorksUI/ExportAssemblyForm.Designer.cs
+++ CodeWorksUI/ExportAssemblyForm.Designer.cs
@@ -28,7 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.titleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(158)))), ((int)(((byte)(223)))));
+            this.titleLabel.Location = new System.Drawing.Point(248, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(618, 106);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Export assembly";
             // 
             // ExportAssemblyForm
             // 
@@ -36,15 +48,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1100, 788);
+            this.Controls.Add(this.titleLabel);
             this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ExportAssemblyForm";
             this.Text = "Export assembly";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLabel;
     }
 }
 
