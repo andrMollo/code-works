20e8b4a46c401dcd00047eaa8aaf531e66d5e0bd
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-18 14:52:32 +0100

    Add method to write log

--- CodeWorksLibrary/Helpers/Logger.cs
+++ CodeWorksLibrary/Helpers/Logger.cs
@@ -1,16 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CodeWorksLibrary.Helpers
 {
-    internal static class Logger
+    internal class Logger
     {
-        internal static void WirteLog(string message)
-        {
+        /// <summary>
+        /// The full path to the log file
+        /// </summary>
+        internal string LogPath { get; set; }
 
+        /// <summary>
+        /// Write a message to file
+        /// </summary>
+        /// <param name="message">The string to be written in the log</param>
+        internal void WirteLog(string message)
+        {
+            string logPath = LogPath;
+
+            using (StreamWriter sw = new StreamWriter(logPath, true))
+            {
+                sw.WriteLine(message);
+            }
+        }
+
+        /// <summary>
+        /// Write a massage to the log file followed by the current date
+        /// </summary>
+        /// <param name="message">The string to be written int the lgo</param>
+        internal void WirteLogWithDate(string message)
+        {
+            string logPath = LogPath;
+
+            using(StreamWriter sw = new StreamWriter(logPath, true))
+            {
+                sw.WriteLine($"{message}; {DateTime.Now}");
+            }
         }
     }
 }
