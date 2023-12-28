950fddcd2b509c7a69867a279da99d8e07af9792
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-12-28 15:58:16 +0100

    Set property to public

--- CodeWorksLibrary/Models/BomElement.cs
+++ CodeWorksLibrary/Models/BomElement.cs
@@ -2,26 +2,26 @@
 
 namespace CodeWorksLibrary.Models
 {
-    internal class BomElement
+    public class BomElement
     {
         /// <summary>
         /// The pointer to the ModelDoc2 object
         /// </summary>
-        internal ModelDoc2 Model {  get; set; }
+        public ModelDoc2 Model {  get; set; }
 
         /// <summary>
         /// The name of the referenced configuration
         /// </summary>
-        internal string Configuration { get; set; }
+        public string Configuration { get; set; }
 
         /// <summary>
         /// The number of instances of the component
         /// </summary>
-        internal double Quantity { get; set; }
+        public double Quantity { get; set; }
 
         /// <summary>
         /// The full path to the component
         /// </summary>
-        internal string Path { get; set; }
+        public string Path { get; set; }
     }
 }
