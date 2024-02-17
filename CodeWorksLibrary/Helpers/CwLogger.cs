ef8f9e6a65c29660a87b71bf203c1967fa008ffd
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-02-17 18:48:38 +0100

    Add log class

---
+++ CodeWorksLibrary/Helpers/CwLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xarial.XCad.Base;
+using Xarial.XCad.Base.Enums;
+
+namespace CodeWorksLibrary.Helpers
+{
+    internal class CwLogger : IXLogger
+    {
+        public void Log(string msg, LoggerMessageSeverity_e severity = LoggerMessageSeverity_e.Information)
+        {
+            this.Trace(msg, GlobalConfig.LoggerName, severity);
+        }
+    }
+}
