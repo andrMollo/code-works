1d7fa3871199019a823f5d4967315aa3b66e2c74
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-02-27 17:30:50 +0100

    Add file validation

--- CodeWorksLibrary/Macros/Files/CloseNoActive.cs
+++ CodeWorksLibrary/Macros/Files/CloseNoActive.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CADBooster.SolidDna;
+using CodeWorksLibrary.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,18 @@ namespace CodeWorksLibrary.Macros.Files
 {
     internal static class CloseNoActive
     {
+        /// <summary>
+        /// Close all the open documents except the active one
+        /// </summary>
         public static void CloseNoActiveMacro()
         {
+            Model model = SolidWorksEnvironment.Application.ActiveModel;
 
+            if (CwValidation.ModelIsOpen(model) == false)
+            {
+                CwMessage.OpenAFile();
+                return;
+            }
         }
     }
 }
