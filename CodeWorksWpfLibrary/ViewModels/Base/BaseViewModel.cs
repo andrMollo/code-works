f73a18aa216d2fb6bef1212c230d1e84e0c9c545
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-02-29 08:54:13 +0100

    Add Base View Model

---
+++ CodeWorksWpfLibrary/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWorksWpfLibrary.ViewModels.Base
+{
+    /// <summary>
+    /// A base view model that fires the property changed event as needed
+    /// </summary>
+    [AddINotifyPropertyChangedInterfaceAttribute]
+    internal class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+    }
+}
