480e96ff99151027fac6c6cfd6c069baa95ec375
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-02-29 08:58:02 +0100

    Add iCloseWindow interface

---
+++ CodeWorksWpfLibrary/Interfaces/ICloseWindow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWorksWpfLibrary.Interfaces
+{
+    internal interface ICloseWindow
+    {
+        /// <summary>
+        /// Action that close the window
+        /// </summary>
+        Action Close { get; set; }
+
+        /// <summary>
+        /// Action that cancel the window
+        /// </summary>
+        Action Cancel { get; set; }
+    }
+}
