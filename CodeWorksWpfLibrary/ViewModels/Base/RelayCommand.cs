ff6f911b14ff34863d8af812ae5df5215d4bfc2a
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2024-02-29 08:55:28 +0100

    Add RelayCommand class

---
+++ CodeWorksWpfLibrary/ViewModels/Base/RelayCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace CodeWorksWpfLibrary.ViewModels.Base
+{
+    /// <summary>
+    /// A basic command that runs an Action
+    /// </summary>
+    internal class RelayCommand : ICommand
+    {
+        #region Private fields
+
+        private readonly Action _action;
+
+        #endregion
+
+        #region Public events
+
+        /// <summary>
+        /// The events that fires when the <see cref="CanExecute(object)"/> value has changed
+        /// </summary>
+        public event EventHandler CanExecuteChanged = (sender, e) => { };
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public RelayCommand(Action action)
+        {
+            _action = action;
+        }
+
+        #endregion
+
+        #region Command methods
+
+        /// <summary>
+        /// A relay command can always execute
+        /// </summary>
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Executes the commands Action
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        #endregion
+    }
+}
